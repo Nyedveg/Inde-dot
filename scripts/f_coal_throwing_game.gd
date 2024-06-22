ad350b51e4302b033cdbf2b5294cb903bb4ac75c
Xunytram <157374865+Xunytram@users.noreply.github.com>
2024-06-22 16:11:23 +0300

    Finished Fire coal thrower game mini first one game
    desc

--- scripts/f_coal_throwing_game.gd
+++ scripts/f_coal_throwing_game.gd
@@ -2,9 +2,16 @@ extends Node
 
 @export var coal_scene: PackedScene
 
+var progress_needed = 100
+var progress_per_coal = 5
+var progress = 0
+var shots_taken = 0
+var coals_hit = 0
+
 var forwards = true
 var is_charging
 var can_shoot = true
+var game_ended = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,10 +28,11 @@ func _process(delta):
 			$CoalBox/ArrowPath/ArrowPoint.progress -= 500*delta
 			if $CoalBox/ArrowPath/ArrowPoint.progress < 10:
 				forwards = true
+	$Hits.text = str(progress)
 	
 func _input(event):
 	# Handle input events
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and not game_ended:
 		if event.button_index == 1:
 			if event.is_pressed():
 				is_charging = true
@@ -36,9 +44,10 @@ func stop_charging():
 	is_charging = false
 	forwards = true
 	if can_shoot:
+		shots_taken += 1
 		for i in range(5):
 			var direction_randomness = randf_range(-PI/16, PI/16)
-			var speed_randomness = randi_range(-50, 50)
+			var speed_randomness = randi_range(-100, 100)
 		
 			var coal = coal_scene.instantiate()
 			var direction = $CoalBox/ArrowPath/ArrowPoint.global_position - $CoalBox/ShootingPoint.global_position
@@ -51,7 +60,19 @@ func stop_charging():
 
 func _on_forge_body_entered(body):
 	body.queue_free()
-
+	progress += progress_per_coal
+	coals_hit += 1
+	if progress >= progress_needed:
+		$WinTimer.start()
+		game_ended = true
+func show_evaluation():
+	var hit_rate = (20 * coals_hit) / shots_taken
+	var score = round(hit_rate / 10)
+	$Score.text = str(score)
 
 func _on_cool_down_timeout():
 	can_shoot = true
+
+
+func _on_win_timer_timeout():
+	show_evaluation()
