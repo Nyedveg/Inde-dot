fd7ced98e1c225990fd0062e9fff805e7ef17722
Xunytram <157374865+Xunytram@users.noreply.github.com>
2024-06-22 15:37:29 +0300

    Bugfiexse

--- scripts/f_coal_throwing_game.gd
+++ scripts/f_coal_throwing_game.gd
@@ -4,6 +4,7 @@ extends Node
 
 var forwards = true
 var is_charging
+var can_shoot = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,39 +14,44 @@ func _ready():
 func _process(delta):
 	if is_charging:
 		if forwards:
-			$CoalBox/ArrowPath.progress += 50*delta
-			if $CoalBox/ArrowPath.progress > 310:
+			$CoalBox/ArrowPath/ArrowPoint.progress += 500*delta
+			if $CoalBox/ArrowPath/ArrowPoint.progress > 310:
 				forwards = false
 		else:
-			$CoalBox/ArrowPath.progress -= 50*delta
-			if $CoalBox/ArrowPath.progress < 10:
+			$CoalBox/ArrowPath/ArrowPoint.progress -= 500*delta
+			if $CoalBox/ArrowPath/ArrowPoint.progress < 10:
 				forwards = true
 	
 func _input(event):
 	# Handle input events
 	if event is InputEventMouseButton:
-		if event.button_index == 0:
+		if event.button_index == 1:
 			if event.is_pressed():
 				is_charging = true
 			else:
 				stop_charging()
 
 func stop_charging():
+	$CoolDown.start()
 	is_charging = false
-	
-	$CoalBox/ArrowPath.progress = 0
 	forwards = true
-	
-	for i in range(5):
-		var direction_randomness = randf_range(-PI/4, PI/4)
-		var speed_randomness = randi_range(-50, 50)
+	if can_shoot:
+		for i in range(5):
+			var direction_randomness = randf_range(-PI/16, PI/16)
+			var speed_randomness = randi_range(-50, 50)
 		
-		var coal = coal_scene.instantiate()
-		var direction = $CoalBox/ArrowPath/ArrowPoint.global_position - $CoalBox/ShootingPoint.global_position
-		direction = direction.normalized()
-		coal.linear_velocity = (direction * (300 + speed_randomness)).rotated(direction_randomness)
-		add_child(coal)
-	
+			var coal = coal_scene.instantiate()
+			var direction = $CoalBox/ArrowPath/ArrowPoint.global_position - $CoalBox/ShootingPoint.global_position
+			direction = direction.normalized()
+			coal.global_position = $CoalBox/ShootingPoint.global_position
+			coal.linear_velocity = (direction * (1200 + speed_randomness)).rotated(direction_randomness)
+			add_child(coal)
+	can_shoot = false
+	$CoalBox/ArrowPath/ArrowPoint.progress = 0
 
 func _on_forge_body_entered(body):
 	body.queue_free()
+
+
+func _on_cool_down_timeout():
+	can_shoot = true
