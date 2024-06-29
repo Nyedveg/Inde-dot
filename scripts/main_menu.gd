eb691f1415979965c1a49d44aaf933c3a1c6ee89
Nyedveg <niedzviegas@nyestudios.com>
2024-06-29 21:38:01 +0300

    Added main menu interactivity

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,19 +1,28 @@
 extends Node
 
+@onready var menu_sfx = $MenuSFX
+@onready var transition = $UI/Transition
+
+var transitioning: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if transitioning:
+		transition.color += Color(0.0, 0.0, 0.0, 0.01)
 
 func _on_start_button_pressed():
+	button_interact()
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
-
 func _on_exit_button_pressed():
+	button_interact()
+	await get_tree().create_timer(1.0).timeout
 	get_tree().quit()
+	
+func button_interact():
+	menu_sfx.play()
+	transitioning = true
